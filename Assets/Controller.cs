fae7679a90df02351b242b536bcb803adaa66fbf
Andre <andre.calder98@gmail.com>
2020-05-18 01:51:52 -0500

    Juan la come

---
+++ Assets/Controller.cs
@@ -0,0 +1,137 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    GameObject[] soldiersP1;
+    Camera[] camerasP1;
+    GameObject[] soldiersP2;
+    Camera[] camerasP2;
+    GameObject active;
+    int currTurn = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        soldiersP1 = GameObject.FindGameObjectsWithTag("soldierP1");
+
+        camerasP1 = new Camera[soldiersP1.Length];
+        for (int i = 0; i < soldiersP1.Length; i++)
+        {
+            camerasP1[i] = soldiersP1[i].transform.Find("cam").gameObject.GetComponent<Camera>();
+        }
+        for (int i = 0; i < camerasP1.Length; i++)
+        {
+            if (i != 0)
+            {
+                camerasP1[i].enabled = false;
+            }
+        }
+
+        soldiersP2 = GameObject.FindGameObjectsWithTag("soldierP2");
+        camerasP2 = new Camera[soldiersP2.Length];
+
+        for (int i = 0; i < soldiersP2.Length; i++)
+        {
+            camerasP2[i] = soldiersP2[i].transform.Find("cam").gameObject.GetComponent<Camera>();
+        }
+        for (int i = 0; i < camerasP2.Length; i++)
+        {
+            if (i != 0)
+            {
+                camerasP2[i].enabled = false;
+            }
+        }
+
+
+
+        active = soldiersP1[soldiersP1.Length - 1];
+        soldiersP1[soldiersP1.Length - 1].GetComponent<Movement>().activate();
+        camerasP1[soldiersP1.Length - 1].enabled = true;
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (currTurn % 2 == 0)
+            {
+                active.GetComponent<Movement>().deactivate();
+                foreach (Camera cam in camerasP1)
+                {
+                    cam.enabled = false;
+                }
+                active = soldiersP2[0];
+                active.GetComponent<Movement>().activate();
+                camerasP2[0].enabled = true;
+            }
+            else
+            {
+                active.GetComponent<Movement>().deactivate();
+                foreach (Camera cam in camerasP2)
+                {
+                    cam.enabled = false;
+                }
+                active = soldiersP1[0];
+                active.GetComponent<Movement>().activate();
+                camerasP1[0].enabled = true;
+            }
+            currTurn++;
+        }
+        if (currTurn % 2 == 0)
+        {
+            if (Input.anyKeyDown)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (Input.GetKeyDown("" + i))
+                    {
+                        if (i <= soldiersP1.Length)
+                        {
+                            active.GetComponent<Movement>().deactivate();
+                            active = soldiersP1[i - 1];
+                            soldiersP1[i - 1].GetComponent<Movement>().activate();
+                            camerasP1[i - 1].enabled = true;
+                            for (int j = 0; j < camerasP1.Length; j++)
+                            {
+                                if (j != i - 1)
+                                {
+                                    camerasP1[j].enabled = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (Input.anyKeyDown)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (Input.GetKeyDown("" + i))
+                    {
+                        if (i <= soldiersP2.Length)
+                        {
+                            active.GetComponent<Movement>().deactivate();
+                            active = soldiersP2[i - 1];
+                            soldiersP2[i - 1].GetComponent<Movement>().activate();
+                            camerasP2[i - 1].enabled = true;
+                            for (int j = 0; j < camerasP2.Length; j++)
+                            {
+                                if (j != i - 1)
+                                {
+                                    camerasP2[j].enabled = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+    float v, h;
+    float cameraRot;
+    float vel;
+
+
+    public bool medic;
+    bool active;
+    bool move;
+    bool shoot;
+    bool gunOut = false;
+    bool hasActivated = false;
+
+
+    public Image Mov;
+    public int MaxMov;
+
+    public Image hpbar; 
+    public float maxhp;
+    public float hp;
+
+    private float initX;
+    private float initZ;
+
+
+    private float currX;
+    private float currZ;
+
+    private float distX;
+    private float distZ;
+
+    private float currDist;
+    private float distPerc;
+
+    Animator anim;
+    GameObject gun;
+    Camera camera;
+    public GameObject healthpack;
+    public RawImage aimcross;
+    float probabilidadDeDsiparo;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        active = false;
+        move = false;
+        this.initX = this.gameObject.transform.position.x;
+        this.initZ = this.gameObject.transform.position.z;
+        this.currX = this.gameObject.transform.position.x;
+        this.currZ = this.gameObject.transform.position.z;
+        gun = this.transform.Find("Bip001").gameObject.transform.Find("Bip001 Pelvis").gameObject.transform.Find("Bip001 Spine").gameObject.transform.Find("Bip001 R Clavicle").gameObject.transform.Find("Bip001 R UpperArm").gameObject.transform.Find("Bip001 R Forearm").gameObject.transform.Find("Bip001 R Hand").gameObject.transform.Find("R_hand_container").gameObject.transform.Find("w_shotgun").gameObject;
+        camera = this.transform.Find("cam").gameObject.GetComponent<Camera>();    
+        aimcross.enabled = false;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        cameraRot = Input.GetAxis("Mouse X");
+
+        this.currX = this.gameObject.transform.position.x;
+        this.currZ = this.gameObject.transform.position.z;
+
+        this.distX = Mathf.Abs(currX-initX);
+        this.distZ = Mathf.Abs(currZ-initZ);
+
+        this.currDist = Mathf.Sqrt((distX*distX) + (distZ * distZ));
+
+
+        if (active && move){
+            hpbar.fillAmount = ((this.hp * 100) / maxhp) / 100;
+            if (MaxMov - currDist <= 0)
+            {
+                move = false;
+            }
+            else
+            {
+                distPerc = (float)currDist / MaxMov;
+                Mov.fillAmount = distPerc;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift)){
+                vel=6;
+            }else{
+                vel=3;
+            }
+            v = Input.GetAxis("Horizontal");
+            h = Input.GetAxis("Vertical"); 
+               
+
+            if(v == 0 && h == 0)
+            {
+                anim.SetFloat("v", 0);
+            }
+            else
+            {
+                anim.SetFloat("v", vel);
+            }
+            transform.Translate(v*Time.deltaTime*vel,0,h*Time.deltaTime*vel);
+            transform.Rotate(0, cameraRot, 0);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anim.SetTrigger("wave");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                if(!gunOut){
+                   anim.SetTrigger("gunO"); 
+                   gun.SetActive(true);
+                   aimcross.enabled = true;
+                    gunOut = true;
+                    anim.SetBool("gunOut", true);
+                }else{
+                    gun.SetActive(false);
+                    aimcross.enabled = false;
+                    gunOut = false;
+                    anim.SetBool("gunOut", false);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                if(medic == true)
+                {
+                    Instantiate(healthpack, this.transform.position + this.transform.forward * 1f, this.transform.rotation);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                if(gunOut){
+                    var ray = camera.ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
+                    if(Physics.Raycast(ray, out hit)){
+                        if(hit.transform.gameObject.tag == "soldierP1" || hit.transform.gameObject.tag == "soldierP2")
+                        {
+                            probabilidadDeDsiparo = Random.Range(0,100);
+                            if(probabilidadDeDsiparo>40){
+                                Debug.Log("Disparo acertado a " + hit.transform.gameObject.tag);
+                                Animator hitAnim = hit.transform.GetComponent<Animator>();
+                                if(hit.transform.gameObject.GetComponent<Movement>().hp > 0)
+                                    hit.transform.gameObject.GetComponent<Movement>().hp -= 50;
+                                hitAnim.SetTrigger("damage");
+
+                            }else{
+                                Debug.Log("Disparo fallado");
+                            }
+                        }
+                    }
+                 }
+                anim.SetTrigger("shoot");
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "healthpack")
+        {
+            this.hp = this.hp + 50;
+            if(this.hp > maxhp)
+            {
+                this.hp = maxhp;
+            }
+            Destroy(other.gameObject);
+        }
+    }
+    public void activate(){
+        
+        active = true;
+        move = true;
+        if (!hasActivated)
+        {
+            hasActivated = true;
+            initX = this.gameObject.transform.position.x;
+            initZ = this.gameObject.transform.position.z;
+            Mov.fillAmount = 0;
+        }
+        print(this.hp);
+        currX = this.gameObject.transform.position.x;
+        currZ = this.gameObject.transform.position.z;
+       
+    }
+    public void deactivate(){
+        active = false;
+        move = false;
+    }
+}
